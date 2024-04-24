4e3f549f3832c4d9bcb9f4e7693ea0fae4ae21ee
Puhi8 <155769684+Puhi8@users.noreply.github.com>
2024-04-24 20:41:12 +0200

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-//var data = await import("./test.json", {with:{type: "json"},})
-//import data from "test.json"
-//var data = fetch("./test.json").then(Response => Response.json()).then(data => console.log(data))
-//import data from './test.json' assert {type: "json"}
-//console.log(data)
-
-const testjson = new Request("./test.json")
-console.log(testjson)
